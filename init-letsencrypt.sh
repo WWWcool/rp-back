97f39d1ccd89463e510db3d228aec800ada506b8
WWW_cool <WWW_cool@inbox.ru>
2019-05-15 17:59:40 +0300

    worked certbot, static, admier, errors on php proxy

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -3,7 +3,7 @@
 domains=(residerp.ru www.residerp.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="WWW_cool@inbox.ru" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
